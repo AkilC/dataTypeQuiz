54fff9d66e3d6c966c555f274c4a4d20738927f8
AkilC <cooperakil@gmail.com>
2020-09-02 13:04:37 -0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+      int yourAge = 21;
+      String zodiacSign = "Virgo";
+      float yourGpa = 3.7f;
+      boolean married = false;
+      System.out.println("Your age is: " + yourAge);
+      System.out.println("Your zodiac sign is: " + zodiacSign);
+      System.out.println("Your gpa is: " + yourGpa);
+      System.out.println("You are married?: " + married);
+    }
+}
